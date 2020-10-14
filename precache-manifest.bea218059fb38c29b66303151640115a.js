4125a5ef558347dc0eb78b8849eb3f8cd1b5a249
makhosi6 <ndondo330@gmail.com>
2020-10-14 10:01:40 +0200

    Updates

---
+++ precache-manifest.bea218059fb38c29b66303151640115a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3faa616d54e2c154077691ef6b4ae57a",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/index.html"
+  },
+  {
+    "revision": "75abbc2be59bbc49b67b",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/css/2.e3b067f2.chunk.css"
+  },
+  {
+    "revision": "56f7ae9228b4aee20f44",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/css/main.cae020fd.chunk.css"
+  },
+  {
+    "revision": "75abbc2be59bbc49b67b",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/js/2.ce8b752e.chunk.js"
+  },
+  {
+    "revision": "56f7ae9228b4aee20f44",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/js/main.535dce83.chunk.js"
+  },
+  {
+    "revision": "81309c7dfa4de968693c",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/js/runtime~main.41ced4e6.js"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/media/Entypo.744ce600.ttf"
+  },
+  {
+    "revision": "140c53a7643ea949007aa9a282153849",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/media/EvilIcons.140c53a7.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/media/Feather.ca9ce9ff.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/media/FontAwesome.b06871f2.ttf"
+  },
+  {
+    "revision": "45a409b1ea9229c6ddf4702b03ef9e6a",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/media/Hands & Heart.45a409b1.webp"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/media/Ionicons.b2e0fc82.ttf"
+  },
+  {
+    "revision": "6acc7d6cffa21f6eda4baaa90e835fd2",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/media/MaterialCommunityIcons.6acc7d6c.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/media/MaterialIcons.a37b0c01.ttf"
+  },
+  {
+    "revision": "8e7f807ef943bff1f6d3c2c6e0f3769e",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/media/Octicons.8e7f807e.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/media/SimpleLineIcons.d2285965.ttf"
+  },
+  {
+    "revision": "0cd24b4e2dde9875ef6462685ca38667",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/media/actor-adult-black-and-white-1040880.0cd24b4e.jpg"
+  },
+  {
+    "revision": "2f3ca802f2177924607fde1927426bc3",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/media/astronaut-astronomy-cosmos-21561.2f3ca802.jpg"
+  },
+  {
+    "revision": "6890c1cceec96f59389bde404789021b",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/media/segoeui.6890c1cc.ttf"
+  },
+  {
+    "revision": "76c30dc8be1105341bb15ec3aada9382",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/media/segoeuil.76c30dc8.ttf"
+  },
+  {
+    "revision": "101eb36c431be6170df5ca8f7eb47ae4",
+    "url": "git@makhosi6.github.com:makhosi6/uncedo.git/static/media/uncedo-logo.101eb36c.svg"
+  }
+]);
