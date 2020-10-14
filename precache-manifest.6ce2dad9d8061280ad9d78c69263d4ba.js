9976062e422f0f4993a12c2ce965f0c827b71be6
makhosi6 <ndondo330@gmail.com>
2020-10-14 10:36:08 +0200

    Updates

---
+++ precache-manifest.6ce2dad9d8061280ad9d78c69263d4ba.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d3dc4afee4aa69ad8ed9333a3df7867",
+    "url": "/Uncedo/web-ui/index.html"
+  },
+  {
+    "revision": "50f2b8b9b08fe91edbc0",
+    "url": "/Uncedo/web-ui/static/css/2.e3b067f2.chunk.css"
+  },
+  {
+    "revision": "212a12ca3ac1b28080ed",
+    "url": "/Uncedo/web-ui/static/css/main.4688d53d.chunk.css"
+  },
+  {
+    "revision": "50f2b8b9b08fe91edbc0",
+    "url": "/Uncedo/web-ui/static/js/2.860639ea.chunk.js"
+  },
+  {
+    "revision": "212a12ca3ac1b28080ed",
+    "url": "/Uncedo/web-ui/static/js/main.57be4dc7.chunk.js"
+  },
+  {
+    "revision": "7136323be2ad4f2c6be4",
+    "url": "/Uncedo/web-ui/static/js/runtime~main.9684e433.js"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/Uncedo/web-ui/static/media/Entypo.744ce600.ttf"
+  },
+  {
+    "revision": "140c53a7643ea949007aa9a282153849",
+    "url": "/Uncedo/web-ui/static/media/EvilIcons.140c53a7.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/Uncedo/web-ui/static/media/Feather.ca9ce9ff.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Uncedo/web-ui/static/media/FontAwesome.b06871f2.ttf"
+  },
+  {
+    "revision": "45a409b1ea9229c6ddf4702b03ef9e6a",
+    "url": "/Uncedo/web-ui/static/media/Hands & Heart.45a409b1.webp"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/Uncedo/web-ui/static/media/Ionicons.b2e0fc82.ttf"
+  },
+  {
+    "revision": "6acc7d6cffa21f6eda4baaa90e835fd2",
+    "url": "/Uncedo/web-ui/static/media/MaterialCommunityIcons.6acc7d6c.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/Uncedo/web-ui/static/media/MaterialIcons.a37b0c01.ttf"
+  },
+  {
+    "revision": "8e7f807ef943bff1f6d3c2c6e0f3769e",
+    "url": "/Uncedo/web-ui/static/media/Octicons.8e7f807e.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/Uncedo/web-ui/static/media/SimpleLineIcons.d2285965.ttf"
+  },
+  {
+    "revision": "0cd24b4e2dde9875ef6462685ca38667",
+    "url": "/Uncedo/web-ui/static/media/actor-adult-black-and-white-1040880.0cd24b4e.jpg"
+  },
+  {
+    "revision": "2f3ca802f2177924607fde1927426bc3",
+    "url": "/Uncedo/web-ui/static/media/astronaut-astronomy-cosmos-21561.2f3ca802.jpg"
+  },
+  {
+    "revision": "6890c1cceec96f59389bde404789021b",
+    "url": "/Uncedo/web-ui/static/media/segoeui.6890c1cc.ttf"
+  },
+  {
+    "revision": "76c30dc8be1105341bb15ec3aada9382",
+    "url": "/Uncedo/web-ui/static/media/segoeuil.76c30dc8.ttf"
+  },
+  {
+    "revision": "101eb36c431be6170df5ca8f7eb47ae4",
+    "url": "/Uncedo/web-ui/static/media/uncedo-logo.101eb36c.svg"
+  }
+]);
